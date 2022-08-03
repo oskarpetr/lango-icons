29584a7c1ddaa63c33f78fcf84ff1a90f4a077ef
oskarpetr <oskar.petr@outlook.com>
2022-08-03 15:01:03 +0200

    Added JSDelivr CDN

--- assets/lango.js
+++ assets/lango.js
@@ -3,6 +3,6 @@ const css = document.createElement('link');
 
 css.rel = "stylesheet";
 css.type = "text/css";
-css.href = "lango.css";
+css.href = "https://cdn.jsdelivr.net/gh/oskarpetr/lango-icons/assets/lango.css";
 
 head.appendChild(css);
