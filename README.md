eb33720f51893b08750fc7eee81be9907a96f4bd
oskarpetr <oskar.petr@outlook.com>
2022-08-03 14:24:35 +0200

    Initial structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lango icons
+Icon family created for designers &amp; developers.
---
+++ assets/lango.js
@@ -0,0 +1,8 @@
+const head = document.querySelector('head');
+const css = document.createElement('link');
+
+css.rel = "stylesheet";
+css.type = "text/css";
+css.href = "lango.css";
+
+head.appendChild(css);
